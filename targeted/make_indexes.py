af4acee31ec40cde6e265792519a2655e992b1c0
lepton-7 <67483910+lepton-7@users.noreply.github.com>
2023-10-04 14:12:01 -0400

    Added progress messages

--- targeted/make_indexes.py
+++ targeted/make_indexes.py
@@ -3,6 +3,7 @@ import click
 from glob import glob
 from pathlib import Path
 from subprocess import run, PIPE
+from tart.utils.helpers import print
 
 
 @click.command()
@@ -22,20 +23,34 @@ from subprocess import run, PIPE
 def main(ref_dir, threads):
     arr = glob(f"{ref_dir}/*.fna")
 
+    print(f"Found these reference files:")
+    for i in arr:
+        print(i)
+
+    print("-------------------------------------------------")
+    print("-------------------------------------------------\n")
+
     for fasta in arr:
         fasta_name = Path(fasta).name[:-4]
         index_dir = f"{fasta[:-4]}_index"
 
         Path(index_dir).mkdir(exist_ok=True, parents=True)
 
-        run(
-            [
-                "hisat2-build",
-                "-p",
-                f"{threads}",
-                f"{fasta}",
-                f"{index_dir}/{fasta_name}_index",
-            ],
-            stdout=PIPE,
-            stderr=PIPE,
-        )
+        print(f"Generating index for {fasta}")
+
+        try:
+            call = run(
+                [
+                    "hisat2-build",
+                    "-p",
+                    f"{threads}",
+                    f"{fasta}",
+                    f"{index_dir}/{fasta_name}_index",
+                ],
+                stdout=PIPE,
+                stderr=PIPE,
+            )
+            print("Success")
+
+        except:
+            print("Failed: ", call.stderr)
