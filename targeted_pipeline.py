c25bf13ad880b08d5450c99b6c33251a420e01e9
lepton-7 <67483910+lepton-7@users.noreply.github.com>
2023-10-04 23:41:03 -0400

    Changed sub command names

--- targeted_pipeline.py
+++ targeted_pipeline.py
@@ -18,8 +18,8 @@ def cli():
 
 cli.add_command(align_reads.main, name="align")
 cli.add_command(filter_BAM_plots.main, name="filter")
-cli.add_command(make_BAMs.main, name="convert_sam")
+cli.add_command(make_BAMs.main, name="convert-sam")
 cli.add_command(make_indexes.main, name="index")
-cli.add_command(make_reference_sequences.main, name="reference_gen")
+cli.add_command(make_reference_sequences.main, name="reference-gen")
 cli.add_command(parse_BAMs.main, name="parse_bam")
 cli.add_command(switch_loc_in_ref.main, name="bounds")
