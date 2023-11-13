a5d6d8e708f1855d9e88c3633ea9aaec432bd12d
lepton-7 <67483910+lepton-7@users.noreply.github.com>
2023-11-13 16:36:15 -0500

    A kunk and a baum alignment

--- validation/alignment/4.align_reads.sh
+++ validation/alignment/4.align_reads.sh
@@ -7,29 +7,29 @@
 #SBATCH --mail-type=BEGIN,END,FAIL
 
 # --------------------------------
-# There are 63 pairs of readsets
-#SBATCH --array=0-63
+# There are 6 pairs of readsets
+#SBATCH --array=0-6
 # --------------------------------
 
 # RUN THIS TO LAUNCH JOB
-## DATECODE="$(date +"%Y-%m-%d_%H-%M-%S")"; sbatch --output=jobs/align_reads.out.c_vibrioides.$DATECODE.%j align_reads.sh
+## DATECODE="$(date +"%Y-%m-%d_%H-%M-%S")"; sbatch --output=jobs/4.align_reads.out.a_kunk.$DATECODE.%j 4.align_reads.sh
 
 # set -x
 set echo on
 
 IDX=$((SLURM_ARRAY_TASK_ID))
 
-DATECODE="2023-09-29_00-47-50"
-DSET="c_vibrioides"
+DATECODE="20231113"
+DSET="a_kunk"
 
 RT="$HOME/packages/tart"
 
-REF_DIR="$RT/validation/alignment/outputs/$DSET/switch_seqs_delta500"
+REF_DIR="$RT/validation/alignment/outputs/$DSET/switch_seqs_delta1000-1000"
 SEQ_DIR="$RT/validation/rna_seq/$DSET"
 
 mkdir -p $REF_DIR
 
-ALIGN_DIR="$REF_DIR/alignments_$DATECODE"
+ALIGN_DIR="$REF_DIR/alignment_$DATECODE"
 mkdir -p $ALIGN_DIR
 
 # Look for all the paired sequence data
@@ -49,7 +49,7 @@ srun tart-targeted align -i $REF_DIR \
     -o $ALIGN_DIR \
     -1 "$SEQ_FILE"_1.fastq.gz \
     -2 "$SEQ_FILE"_2.fastq.gz \
-    --readset-name $(basename "$SEQ_FILE") \
+    --readpair-name $(basename "$SEQ_FILE") \
     -t \
     --no-unal \
     --score-min L,0,-0.6
