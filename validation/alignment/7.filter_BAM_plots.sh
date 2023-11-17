3e1ffa5bd593f0f677682dfb582dfca55a20b313
lepton-7 <67483910+lepton-7@users.noreply.github.com>
2023-11-17 16:16:58 -0500

    FIltered all outputs

--- validation/alignment/7.filter_BAM_plots.sh
+++ validation/alignment/7.filter_BAM_plots.sh
@@ -1,27 +1,51 @@
 #!/bin/bash
 
-#SBATCH --time=00:05:00
-#SBATCH --nodes=2 --cpus-per-task=1
+#SBATCH --time=00:50:00
+#SBATCH --nodes=3 --cpus-per-task=1
 
 #SBATCH --account=PDS0325
 #SBATCH --mail-type=BEGIN,END,FAIL
 
 # RUN THIS TO LAUNCH JOB
-## sbatch --output=jobs/7.filter_BAM_plots.out.$(date +"%Y-%m-%d_%H-%M-%S").%j 7.filter_BAM_plots.sh
+## sbatch --output=jobs/7.all.filter_BAM_plots.out.$(date +"%Y-%m-%d_%H-%M-%S").%j 7.filter_BAM_plots.sh
 
-set -x
+# set -x
 set echo on
 
 RT="$HOME/packages/tart"
-DSET="a_kunk"
 
-D_ROOT="$RT/validation/alignment/outputs/$DSET"
+DSETS=(
+    "a_baum"
+    "a_rabiei"
+    "c_vibrioides"
+    "m_tuber"
+    "s_elon"
+    "s_spcc6803"
+    "a_fischeri_ES114"
+    "b_sub_168"
+    "e_coli"
+    "p_fluor"
+    "s_enter_typh"
+    "x_albi"
+    "a_kunk"
+    "b_theta"
+    "k_pneum"
+    "s_coelicolor"
+    "s_sanguinis"
+)
 
-PICKLE_ROOT="$D_ROOT/plots/picks"
-SAVE_ROOT="$D_ROOT/plots/"
+for DSET in ${DSETS[@]}; do
 
-echo "Filetering plots for $DSET"
+    D_ROOT="$RT/validation/alignment/outputs/$DSET"
 
-mpiexec tart-targeted filter -i $PICKLE_ROOT -o $SAVE_ROOT --ext-prop -0.3 1.0 --conv --min-cov-depth 25
+    PICKLE_ROOT="$D_ROOT/plots/picks.tar.gz"
+    SAVE_ROOT="$D_ROOT/plots/"
 
-echo "Finished filtering outputs for $DSET into $SAVE_ROOT"
+    echo "Filtering plots for $DSET"
+
+    mpiexec tart-targeted filter -i $PICKLE_ROOT -o $SAVE_ROOT --ext-prop -0.3 1.0 --conv --min-cov-depth 25
+
+    echo "Finished filtering outputs for $DSET into $SAVE_ROOT"
+    echo "----------------------------------------------------"
+    echo
+done
