b3aedd674b476713f1cb91b8a8772d07ec571936
lepton-7 <67483910+lepton-7@users.noreply.github.com>
2023-11-17 11:38:37 -0500

    Removed older plots and picks

--- validation/alignment/6.parse_BAMs.sh
+++ validation/alignment/6.parse_BAMs.sh
@@ -1,26 +1,52 @@
 #!/bin/bash
 
-#SBATCH --time=00:10:00
+#SBATCH --time=00:50:00
 #SBATCH --nodes=2 --cpus-per-task=1
 
 #SBATCH --account=PDS0325
 #SBATCH --mail-type=BEGIN,END,FAIL
 
 # RUN THIS TO LAUNCH JOB
-## sbatch --output=jobs/6.parse_BAMs.out.$(date +"%Y-%m-%d_%H-%M-%S").%j 6.parse_BAMs.sh
+## sbatch --output=jobs/6.all.parse_BAMs.out.$(date +"%Y-%m-%d_%H-%M-%S").%j 6.parse_BAMs.sh
 
 set echo on
 
 RT="$HOME/packages/tart"
 
 DATECODE="20231113"
-DSET="x_albi"
 
-BAM_DIR="$RT/validation/alignment/outputs/$DSET/switch_seqs_delta1000-1000/alignment_$DATECODE"
-SAVE_ROOT="$RT/validation/alignment/outputs/$DSET/plots/picks"
-BOUNDS_PATH="$RT/validation/alignment/outputs/$DSET/switch_seqs_delta1000-1000/rowid_to_bounds.json"
+DSETS=(
+    "a_baum"
+    "a_rabiei"
+    "c_vibrioides"
+    "m_tuber"
+    "s_elon"
+    "s_spcc6803"
+    "a_fischeri_ES114"
+    "b_sub_168"
+    "e_coli"
+    "p_fluor"
+    "s_enter_typh"
+    "x_albi"
+    "a_kunk"
+    "b_theta"
+    "k_pneum"
+    "s_coelicolor"
+    "s_sanguinis"
+)
 
-mpiexec tart-targeted parse-bam -i $BAM_DIR -o $SAVE_ROOT --bounds-file $BOUNDS_PATH --picks --allow-soft-clips &&
-    wait
+for DSET in ${DSETS[@]}; do
 
-echo "Finished BAM parsing for $DSET into $SAVE_ROOT"
+    BAM_DIR="$RT/validation/alignment/outputs/$DSET/switch_seqs_delta1000-1000/alignment_$DATECODE"
+    SAVE_ROOT="$RT/validation/alignment/outputs/$DSET/plots/picks"
+    BOUNDS_PATH="$RT/validation/alignment/outputs/$DSET/switch_seqs_delta1000-1000/rowid_to_bounds.json"
+
+    echo "Starting BAM parsing for $DSET"
+
+    mpiexec tart-targeted parse-bam -i $BAM_DIR -o $SAVE_ROOT --bounds-file $BOUNDS_PATH --picks --allow-soft-clips &&
+        wait
+
+    echo "Finished BAM parsing for $DSET into $SAVE_ROOT"
+    echo "----------------------------------------------"
+    echo
+done
