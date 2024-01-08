a7a944d4cce8bb4fed74e345cfefc8aed66ecbf1
lepton-7 <67483910+lepton-7@users.noreply.github.com>
2024-01-07 23:53:01 -0500

    Cumulative results with run metadata

---
+++ validation/alignment/results.py
@@ -0,0 +1,35 @@
+# %%
+import pandas as pd
+
+from glob import glob
+from pathlib import Path
+
+# %%
+res_paths = glob("outputs/*/plots/characteristics.*")
+cond_path = "../rna_seq/run_conditions.csv"
+
+cond_df = pd.read_csv(cond_path)
+# %%
+# Compile output characteristics into one file
+
+df = pd.DataFrame()
+for dfpath in res_paths:
+    d2 = pd.read_csv(dfpath)
+    bug = Path(dfpath).parent.parent.stem
+    buglist = [bug for i in range(len(d2))]
+    d2["dataset"] = buglist
+
+    df = pd.concat([df, d2], ignore_index=True)
+
+# %%
+merged = df.merge(
+    cond_df,
+    left_on="transcriptome",
+    right_on="Run",
+    right_index=False,
+    left_index=False,
+)
+merged.drop(columns=["Run"], inplace=True)
+
+# %%
+merged.to_csv("results.csv", index=False)
