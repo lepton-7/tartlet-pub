9973ac1cf0c4f878889ea762299b93b844464481
lepton-7 <67483910+lepton-7@users.noreply.github.com>
2024-01-10 04:44:48 -0500

    Added aliases to conditions and riboswitch rowids

--- validation/plotting/data_morphing.py
+++ validation/plotting/data_morphing.py
@@ -1,4 +1,5 @@
 # %%
+from typing import Any
 import pandas as pd
 
 from collections import defaultdict
@@ -13,6 +14,34 @@ results_df = pd.read_csv(cum_results_path)
 # %% --------------------------------------------------------------------------
 # -----------------------------------------------------------------------------
 # -----------------------------------------------------------------------------
+# Make aliases for conditions and riboswitch loci
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+alias_dict = {}
+
+for dset in pd.unique(results_df["dataset"]):
+    subset = results_df[results_df["dataset"] == dset]
+    prev = ""
+    count = 1
+    for rowid in pd.unique(subset["rowid"]):
+        if prev != rowid.split("#")[0]:
+            prev = rowid.split("#")[0]
+            count = 1
+        else:
+            count += 1
+
+        alias_dict[rowid] = f"{dset}_{prev}_{count}"
+
+    prev = ""
+    count = 1
+    for cond in pd.unique(subset["condition"]):
+        alias_dict[f"{dset}#{cond}"] = [dset, f"cond_{count}"]
+        count += 1
+
+
+# %% --------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Tally results by counting decisions
 # -----------------------------------------------------------------------------
 # -----------------------------------------------------------------------------
@@ -21,7 +50,10 @@ tally = defaultdict(lambda: defaultdict(dict))
 
 for _, row in results_df.iterrows():
     id = row["rowid"]
+    id_alias = alias_dict[row["rowid"]]
+    dset = row["dataset"]
     cond = row["condition"]
+    cond_alias = alias_dict[f"{dset}#{row['condition']}"][1]
     dec = row["decision"]
     geo = row["study"]
 
@@ -29,7 +61,13 @@ for _, row in results_df.iterrows():
         tally[f"{id}"][f"{cond}"]["dataset"]
     except:
         tally[f"{id}"][f"{cond}"].update(
-            {"dataset": row["dataset"], "pass": 0, "fail": 0, "incon": 0, "geo": geo}
+            {
+                "geo": geo,
+                "dataset": row["dataset"],
+                "pass": 0,
+                "fail": 0,
+                "incon": 0,
+            }
         )
 
     if dec != "pass" and dec != "fail":
@@ -39,20 +77,24 @@ for _, row in results_df.iterrows():
     tally[f"{id}"][f"{cond}"][dec] += 1
     # except KeyError:
 
-inter = []
+inter: list[dict[Any, Any]] = []
 
 for rowid, conds in tally.items():
     for cond, decs in conds.items():
         t = {}
+        t["switch_id"] = rowid
+        t["condition"] = cond
+
         for decision, val in decs.items():
-            t["switch_id"] = rowid
-            t["condition"] = cond
             t[decision] = val
+
+        t["switch_alias"] = alias_dict[rowid]
+        t["condition_alias"] = alias_dict[f"{decs['dataset']}#{cond}"][1]
         inter.append(t)
 
 tally_df = pd.DataFrame.from_dict(inter)
 
 # %%
-tally_df.to_csv("./data/tally.csv", index=False)
+tally_df.to_csv("./data/tally_aliased.csv", index=False)
 
 # %%
