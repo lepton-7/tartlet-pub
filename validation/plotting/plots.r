00db09907a16ab6701821be92f37b75b9369e4a9
lepton-7 <67483910+lepton-7@users.noreply.github.com>
2025-01-21 08:43:47 -0700

    Fix counting in legend text
    Signed-off-by: lepton-7 <67483910+lepton-7@users.noreply.github.com>

--- validation/plotting/plots.r
+++ validation/plotting/plots.r
@@ -466,16 +466,17 @@
 
     {
         # This is the legend for panel b
+        sum_neg_nr <- sum(inf_df$inactive) - sum(inf_df$incon_disagree) - sum(inf_df$incon_agree)
 
         leg_df <- data.frame(
             tool = c("Y", "Y", "Y", "NA", "NA", "NA"),
             litresult = c("Y", "Trans", "NA", "Y", "Trans", "NA"),
             colkey = c("act_ag", "act_dis", "act", "incon_dis", "incon_ag", "nov"),
-            keycounts = c(sum(inf_df$active_agree), sum(inf_df$active_disagree), sum(inf_df$active), sum(inf_df$incon_disagree), sum(inf_df$incon_agree), sum(inf_df$inactive))
+            keycounts = c(sum(inf_df$active_agree), sum(inf_df$active_disagree), sum(inf_df$active), sum(inf_df$incon_disagree), sum(inf_df$incon_agree), sum_neg_nr)
         )
         leg <- ggplot(leg_df, aes(x = tool, y = litresult, fill = colkey)) +
             geom_tile() +
-            geom_text(aes(label=keycounts)) +
+            geom_text(aes(label = keycounts), size = 6, fontface = "bold") +
             def_theme +
             theme(
                 axis.text.x = element_text(size = 20),
